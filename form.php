86975c2ac7fc5f9c7d8c58e9ab32cc67dc481903
github user name <fujipurati+hogehoge@gmail.com>
2020-02-04 05:53:42 +0900

    バリデーションを追記
    値がない場合何も表示されないため

--- form.php
+++ form.php
@@ -1,9 +1,13 @@
 <?php
 // 値を受け取る
 $hoge = $_POST['hoge'];
-
 $hello = "hello";
-// あいさつ
+if ('' == $hoge) {
+  // 未入力なら警告を出力
+  echo "値が未入力です";
+} else {
+  // あいさつ
 echo "$hello<br>\n";
-// 値を表示する
-echo htmlspecialchars($hoge, ENT_QUOTES, `UTF-8`);
+  // 値を表示する
+  echo htmlspecialchars($hoge, ENT_QUOTES, 'UTF-8');
+}
